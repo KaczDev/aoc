8c9466f25b6486b86d7443d4881335fa3ab00dd4
KaczDev <71774507+KaczDev@users.noreply.github.com>
2025-12-10 09:18:56 +0000

    kudos to a reddit user that had a clean solution that made me understand the polygon solution

--- rust_2025/src/bin/day9.rs
+++ rust_2025/src/bin/day9.rs
@@ -1,29 +1,30 @@
 use anyhow::Result;
-use std::{fmt::Display, fs, str::FromStr, time::Instant};
+use itertools::Itertools;
+use std::{cmp, collections::HashSet, fmt::Display, fs, str::FromStr, time::Instant};
 
 type Input<T> = Vec<T>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct Position {
+struct Corner {
     col: isize,
     row: isize,
 }
 
-impl Position {
-    fn area(&self, r2: &Position) -> isize {
+impl Corner {
+    fn area(&self, r2: &Corner) -> isize {
         let r1 = self;
         let a = (r2.col - r1.col).abs() + 1;
         let b = (r2.row - r1.row).abs() + 1;
         a * b
     }
 }
-impl Display for Position {
+impl Display for Corner {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Position({},{})", self.col, self.row)
     }
 }
 
-impl FromStr for Position {
+impl FromStr for Corner {
     type Err = anyhow::Error;
 
     // expects s to be "x,y"
@@ -40,7 +41,7 @@ impl FromStr for Position {
 }
 
 fn solve(file_name: &str) -> Result<isize> {
-    let input: Input<Position> = read_input(file_name)?;
+    let input: Input<Corner> = read_input(file_name)?;
     let mut max_area = isize::MIN;
     input.iter().for_each(|r1| {
         input.iter().for_each(|r2| {
@@ -54,27 +55,69 @@ fn solve(file_name: &str) -> Result<isize> {
 }
 
 fn solve_2(file_name: &str) -> Result<isize> {
-    // TODO: Brute-force ain't it, gotta do something with cord compression or polygon
-    let input: Input<Position> = read_input(file_name)?;
-    // println!("{:?}", input);
-    let mut max_x = 0;
-    let mut max_y = 0;
-    for i in &input {
-        if i.col > max_x {
-            max_x = i.col + 1;
+    let input: Input<Corner> = read_input(file_name)?;
+    // polygon's perimeter
+    let mut polygon: HashSet<Corner> = HashSet::new();
+    input.iter().circular_tuple_windows().for_each(|(r1, r2)| {
+        let min_row = cmp::min(r1.row, r2.row);
+        let max_row = cmp::max(r1.row, r2.row);
+        let min_col = cmp::min(r1.col, r2.col);
+        let max_col = cmp::max(r1.col, r2.col);
+        if r1.row == r2.row {
+            for col in min_col..=max_col {
+                polygon.insert(Corner {
+                    row: r1.row,
+                    col: col,
+                });
+            }
         }
-        if i.row > max_y {
-            max_y = i.row + 1;
+        if r1.col == r2.col {
+            for row in min_row..=max_row {
+                polygon.insert(Corner {
+                    row: row,
+                    col: r1.col,
+                });
+            }
+        }
+    });
+    let mut rectangles: Vec<(Corner, Corner, isize)> = input
+        .iter()
+        .combinations(2)
+        .map(|comb| {
+            let r1 = comb[0];
+            let r2 = comb[1];
+            let a = r1.area(r2);
+            (*r1, *r2, a)
+        })
+        .collect();
+    //sort rectangles by area
+    rectangles.sort_by(|r1, r2| r2.2.cmp(&r1.2));
+    for (r1, r2, area) in rectangles {
+        if is_valid_rectangle(&polygon, &r1, &r2) {
+            return Ok(area);
         }
     }
 
     Ok(0)
 }
 
-fn read_input(file_name: &str) -> Result<Input<Position>> {
+fn is_valid_rectangle(polygon: &HashSet<Corner>, r1: &Corner, r2: &Corner) -> bool {
+    let min_row = cmp::min(r1.row, r2.row);
+    let max_row = cmp::max(r1.row, r2.row);
+    let min_col = cmp::min(r1.col, r2.col);
+    let max_col = cmp::max(r1.col, r2.col);
+    for p in polygon {
+        if p.row > min_row && p.row < max_row && p.col > min_col && p.col < max_col {
+            return false;
+        }
+    }
+    true
+}
+
+fn read_input(file_name: &str) -> Result<Input<Corner>> {
     Ok(fs::read_to_string(file_name)?
         .lines()
-        .map(|line| line.parse::<Position>().expect("culdnt parse"))
+        .map(|line| line.parse::<Corner>().expect("culdnt parse"))
         .collect())
 }
 
