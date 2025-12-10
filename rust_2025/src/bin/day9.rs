600095b3c582f9b57e4d2a58fe0d6c1d024b57c0
KaczDev <71774507+KaczDev@users.noreply.github.com>
2025-12-10 00:28:51 +0000

    d9p1, p2 todo

---
+++ rust_2025/src/bin/day9.rs
@@ -0,0 +1,116 @@
+use anyhow::Result;
+use std::{fmt::Display, fs, str::FromStr, time::Instant};
+
+type Input<T> = Vec<T>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Position {
+    col: isize,
+    row: isize,
+}
+
+impl Position {
+    fn area(&self, r2: &Position) -> isize {
+        let r1 = self;
+        let a = (r2.col - r1.col).abs() + 1;
+        let b = (r2.row - r1.row).abs() + 1;
+        a * b
+    }
+}
+impl Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Position({},{})", self.col, self.row)
+    }
+}
+
+impl FromStr for Position {
+    type Err = anyhow::Error;
+
+    // expects s to be "x,y"
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let s = s.split(",").collect_vec();
+        let x = s[0]
+            .parse::<isize>()
+            .expect(&format!("Couldn't parse {}", s[0]));
+        let y = s[1]
+            .parse::<isize>()
+            .expect(&format!("Couldn't parse {}", s[1]));
+        Ok(Self { col: x, row: y })
+    }
+}
+
+fn solve(file_name: &str) -> Result<isize> {
+    let input: Input<Position> = read_input(file_name)?;
+    let mut max_area = isize::MIN;
+    input.iter().for_each(|r1| {
+        input.iter().for_each(|r2| {
+            let area = r1.area(&r2);
+            if area > max_area {
+                max_area = area;
+            }
+        });
+    });
+    Ok(max_area)
+}
+
+fn solve_2(file_name: &str) -> Result<isize> {
+    // TODO: Brute-force ain't it, gotta do something with cord compression or polygon
+    let input: Input<Position> = read_input(file_name)?;
+    // println!("{:?}", input);
+    let mut max_x = 0;
+    let mut max_y = 0;
+    for i in &input {
+        if i.col > max_x {
+            max_x = i.col + 1;
+        }
+        if i.row > max_y {
+            max_y = i.row + 1;
+        }
+    }
+
+    Ok(0)
+}
+
+fn read_input(file_name: &str) -> Result<Input<Position>> {
+    Ok(fs::read_to_string(file_name)?
+        .lines()
+        .map(|line| line.parse::<Position>().expect("culdnt parse"))
+        .collect())
+}
+
+fn main() -> Result<()> {
+    let files = ["./inputs/day9.test", "./inputs/day9.prod"];
+    println!("# Part 1");
+    for file in files {
+        let now = Instant::now();
+        let res = solve(file)?;
+        println!("{}: {} in {}ms", file, res, now.elapsed().as_millis());
+    }
+    println!("# Part 2");
+    for file in files {
+        let now = Instant::now();
+        let res = solve_2(file)?;
+        println!("{}: {} in {}ms", file, res, now.elapsed().as_millis());
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // #[test]
+    // fn part_1() {
+    //     let file = "./inputs/day9.test";
+    //     let result = solve(file).unwrap();
+    //     let expected = 50;
+    //     assert_eq!(result, expected)
+    // }
+
+    #[test]
+    fn part_2() {
+        let file = "./inputs/day9.test";
+        let result = solve_2(file).unwrap();
+        let expected = 24;
+        assert_eq!(result, expected)
+    }
+}
